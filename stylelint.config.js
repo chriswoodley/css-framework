15d8f8acc1f67304413b2a6db9eeab0431c94cd0
Chris Woodley <chriswoodley@me.com>
2020-05-31 16:48:21 -0400

    update stylelint standards

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,23 +1,6 @@
 module.exports = {
-  extends: 'stylelint-config-standard',
-  rules: {
-    indentation: 2,
-
-    'at-rule-no-unknown': [
-      true,
-      {
-        'ignoreAtRules': ['extend', 'include', 'if', 'return', 'function', 'each', 'mixin']
-      }
-    ],
-
-    'at-rule-no-vendor-prefix': true,
-    'media-feature-name-no-vendor-prefix': true,
-    'property-no-vendor-prefix': true,
-    'selector-no-vendor-prefix': true,
-    'value-no-vendor-prefix': true,
-    'max-nesting-depth': 3,
-
-    'selector-max-compound-selectors': 3,
-    'selector-max-specificity': '0,2,1'
-  }
+  "extends": [
+    "stylelint-config-recommended-scss",
+    "stylelint-config-rational-order"
+  ]
 }
