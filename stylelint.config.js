f2c4e4f1046c78e524d1adc7368f119397af6cb2
Chris Woodley <chriswoodley@me.com>
2020-06-03 19:59:18 -0400

    update styleling config

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,16 @@
 module.exports = {
-  "extends": [
-    "stylelint-config-recommended-scss",
-    "stylelint-config-rational-order"
-  ]
+  extends: [
+    'stylelint-config-recommended-scss',
+    'stylelint-config-rational-order'
+  ],
+  rules: {
+    'scss/at-import-no-partial-leading-underscore': true,
+    'scss/at-import-partial-extension-blacklist': ['scss'],
+    'scss/at-rule-no-unknown': true,
+    'scss/dollar-variable-colon-space-after': 'always',
+    'scss/dollar-variable-colon-space-before': 'never',
+    'scss/map-keys-quotes': 'always',
+    'scss/operator-no-unspaced': true,
+    'string-quotes': 'single',
+  }
 }
